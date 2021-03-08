b5d45a7bb298fac4f8f14c2996ce7eaa8e67d332
raphaelDuff <raphaelprates@outlook.com>
2021-03-08 19:11:07 -0300

    Coroutine was added to retrofit

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -37,8 +37,8 @@ private val retrofit = Retrofit.Builder()
 
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties():
-            Call<List<MarsProperty>>
+    suspend fun getProperties():
+            List<MarsProperty>
 }
 
 object MarsApi {
